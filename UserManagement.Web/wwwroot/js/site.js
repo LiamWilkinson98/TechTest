7bea2a1f7135563be5766e0c8b8bfe9e313323c7
Liam Wilkinson <liam.wilkinson.ds@googlemail.com>
2024-06-11 15:43:50 +0100

    Using JQuery datepicker for date of birth now. Validation added to only allow letters for forename and surname some CSS added to make EditUser and AddUser pages neater

--- UserManagement.Web/wwwroot/js/site.js
+++ UserManagement.Web/wwwroot/js/site.js
@@ -1,4 +1,30 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+
+
+
+$(document).ready(function () {
+
+    //only accepts letters for forename and surname on AddUser and EditUser
+    $('#forename').keyup(function () {
+        var $letter = $(this);
+        $letter.val($letter.val().replace(/[^a-zA-Z]/, function () { alert('Please use only letters.'); return ''; }));
+    });
+    $('#surname').keyup(function () {
+        var $letter = $(this);
+        $letter.val($letter.val().replace(/[^a-zA-Z]/, function () { alert('Please use only letters.'); return ''; }));
+    });
+
+    //year range is from 1904 to current day
+    $(function () {
+        $("#dateOfBirth").datepicker({
+            changeMonth: true,
+            changeYear: true,
+            yearRange: "-120:+0",
+            maxDate: "0",
+            dateFormat: "dd/mm/yy"
+        });
+    });
+
+});
